861f73193cd450a474eea6f70d5ab1061d457a51
shamsshaikh <shams.shaikh@softwaremotorcorp.com>
2017-01-15 18:15:21 -0800

    added authorizations 22_1

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116014254) do
+ActiveRecord::Schema.define(version: 20170116015856) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

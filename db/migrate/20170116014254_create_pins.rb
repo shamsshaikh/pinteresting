82fd153e76e69e02a72d2cb890491e2d1108feed
shamsshaikh <shams.shaikh@softwaremotorcorp.com>
2017-01-15 17:54:40 -0800

    built pin scaffold

---
+++ db/migrate/20170116014254_create_pins.rb
@@ -0,0 +1,10 @@
+class CreatePins < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pins do |t|
+      t.string :description
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end

b1808706f81962b6eb1966a75135911f16e1d6dd
shamsshaikh <shams.shaikh@softwaremotorcorp.com>
2017-01-16 22:41:45 -0800

    Add name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ApplicationRecord
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]	
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+	validates :image, presence: true
+  validates :description, presence: true
 end

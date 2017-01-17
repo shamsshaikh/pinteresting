22364cba6a4134e91d1baf2e45b03aa332963f8b
shamsshaikh <shams.shaikh@softwaremotorcorp.com>
2017-01-16 22:11:13 -0800

    fix to db, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116032804) do
+ActiveRecord::Schema.define(version: 20170117060755) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
